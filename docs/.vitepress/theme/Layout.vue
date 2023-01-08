01a1d79c214c91b5724283905c846a941c12668e
Web3HackerDAO <web3hackerdao@gmail.com>
2023-01-08 11:24:45 +0800

    ✨ finish home

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,6 +1,8 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import Web3 from './components/Web3.vue'
+import HomeFeaturesBefore from './components/HomeFeaturesBefore.vue'
+import HomeFeaturesAfter from './components/HomeFeaturesAfter.vue'
 const { Layout } = DefaultTheme
 </script>
 
@@ -10,6 +12,12 @@ const { Layout } = DefaultTheme
       <ClientOnly>
         <Web3 />
       </ClientOnly>
+    </template>
+    <template #home-features-before>
+      <HomeFeaturesBefore />
+    </template>
+    <template #home-features-after>
+      <HomeFeaturesAfter />
     </template>
   </Layout>
 </template>
