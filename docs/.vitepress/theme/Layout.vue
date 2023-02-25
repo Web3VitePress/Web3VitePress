d296afcfb54d634ca18fe4e35c87c5b44d44c2cb
Web3HackerDAO <web3hackerdao@gmail.com>
2023-02-25 11:18:50 +0800

    ✨ fix team page

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import Web3 from './components/Web3.vue'
+import Team from './components/Team.vue'
 const { Layout } = DefaultTheme
 </script>
 
@@ -9,6 +10,11 @@ const { Layout } = DefaultTheme
     <template #aside-outline-after>
       <ClientOnly>
         <Web3 />
+      </ClientOnly>
+    </template>
+    <template #home-features-before>
+      <ClientOnly>
+        <Team v-if="$frontmatter.team" />
       </ClientOnly>
     </template>
   </Layout>
